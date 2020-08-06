84b7b805592092dcf1da8dc1bc3e85fef9eda3df
Daddy010 <65995650+Daddy010@users.noreply.github.com>
2020-08-06 10:08:35 +0200

    Given user the ability to choose Planetside 2 Directory Path by himself

--- Swapper.java
+++ Swapper.java
@@ -1,13 +1,18 @@
 package ps2.iniswapper;
 
 import java.awt.FlowLayout;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public class Swapper {
@@ -16,34 +21,48 @@ public class Swapper {
 	public static JButton air;
 	public static JButton tank;
 	public static JButton infantry;
+	public static String content;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new File("C:\\PS2SwapperSettings").mkdir();
+		new File("C:\\PS2SwapperSettings\\inis").mkdir();
 
-		new File("C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\inis").mkdir();
-		StartGui();
+		String file_name = "C:\\PS2SwapperSettings\\path.txt";
+		File f = new File(file_name);
+		if (f.exists() && !f.isDirectory()) {
+			StartGui();
+		} else {
+			pathfile();
+		}
+
 	}
 
 	public static void BuildGui() {
+
+		String filename = "C:\\PS2SwapperSettings\\path.txt";
+
+		try {
+			content = new String(Files.readAllBytes(Paths.get(filename)));
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 
 		frame = new JFrame("INI SWAPPER by Daddy010");
 		frame.setSize(350, 100);
 		frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
 		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 
 		air = new JButton("Use Air ini");
 		air.setFocusable(false);
 		air.addActionListener(e -> {
 			try {
-				Files.copy(new File(
-						"C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\inis\\air.ini")
-								.toPath(),
-						new File(
-								"C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\UserOptions.ini")
-										.toPath(),
-						StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(new File("C:\\PS2SwapperSettings\\inis\\air.ini").toPath(),
+						new File(content + "\\UserOptions.ini").toPath(), StandardCopyOption.REPLACE_EXISTING);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -54,13 +73,8 @@ public class Swapper {
 		tank.setFocusable(false);
 		tank.addActionListener(e -> {
 			try {
-				Files.copy(new File(
-						"C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\inis\\tank.ini")
-								.toPath(),
-						new File(
-								"C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\UserOptions.ini")
-										.toPath(),
-						StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(new File("C:\\PS2SwapperSettings\\inis\\tank.ini").toPath(),
+						new File(content + "\\UserOptions.ini").toPath(), StandardCopyOption.REPLACE_EXISTING);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -71,13 +85,8 @@ public class Swapper {
 		infantry.setFocusable(false);
 		infantry.addActionListener(e -> {
 			try {
-				Files.copy(new File(
-						"C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\inis\\infantry.ini")
-								.toPath(),
-						new File(
-								"C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\PlanetSide 2\\UserOptions.ini")
-										.toPath(),
-						StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(new File("C:\\PS2SwapperSettings\\inis\\infantry.ini").toPath(),
+						new File(content + "\\UserOptions.ini").toPath(), StandardCopyOption.REPLACE_EXISTING);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -99,4 +108,21 @@ public class Swapper {
 
 		});
 	}
+
+	public static void pathfile() {
+
+		String input = JOptionPane.showInputDialog("Please insert the path to your Planetside 2 Directory");
+		File writefile = new File("C:\\PS2SwapperSettings\\path.txt");
+
+		try {
+			FileWriter writer = new FileWriter(writefile);
+			writer.write(input.toString());
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
